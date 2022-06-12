42d46b2b26493f1c52423e7a3289fb0fe7687e17
Pistrie <sylvester.roos@xs4all.nl>
2022-06-12 10:31:34 +0200

    added extra help

--- linux-iso-downloader.sh
+++ linux-iso-downloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # exit when error
 
-cat << "EOF"
+cat <<"EOF"
      /$$       /$$                                     /$$$$$$  /$$$$$$   /$$$$$$           
     | $$      |__/                                    |_  $$_/ /$$__  $$ /$$__  $$          
     | $$       /$$ /$$$$$$$  /$$   /$$ /$$   /$$        | $$  | $$  \__/| $$  \ $$  /$$$$$$$
@@ -13,25 +13,41 @@ cat << "EOF"
     https://gitlab.com/Pistrie/linux-iso-downloading-setup
 EOF
 
+mkdir openvpn # this is where the ovpn files need to go
+
 args=$1
 
 stop_script() {
-  docker-compose down --remove-orphans || true
+  docker-compose down || true
+}
+
+after_start() {
+  echo ""
+  echo "script finished correctly"
+  echo ""
 }
 
 if [[ $args == 'start' ]]; then
   stop_script
   docker-compose up --force-recreate -d
+  after_start
 elif [[ $args == 'start:vpn' ]]; then
   stop_script
-  docker-compose -f docker-compose.yaml -f vpn/docker-compose.yaml up --force-recreate -d
+  docker-compose -f docker-compose.yaml -f docker-compose.vpn.yaml up --force-recreate -d
+  after_start
 elif [[ $args == 'stop' ]]; then
   stop_script
   echo ""
   echo "script correctly stopped"
 elif [[ $args == 'update' ]]; then
   docker-compose pull
+elif [[ $args == 'help' ]]; then
+  echo "use start if you want to start the containers without a vpn connection"
+  echo "use start:vpn if you want to start the containers with a vpn connection"
+  echo "    place your openvpn config files in ./openvpn"
+  echo "use stop if you want to stop all the containers"
+  echo "use update if you want to download the newest version of all the containers"
 else
   echo "unknown command: $args"
-  echo "use [start | start:vpn | stop | update]"
+  echo "use [start | start:vpn | stop | update | help]"
 fi
